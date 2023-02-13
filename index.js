7d60d1bb1c1d7d569da983c3534cff289f7adb7b
Enssis <Enssis@protonmail.com>
2023-02-13 23:46:17 +0100

    Added mouse wheel control

--- index.js
+++ index.js
@@ -6,14 +6,30 @@ for(let i = 0; i < nav.length; i++) {
     let nav_act = nav[i]; 
     nav_act.onclick = () => {
         if(i === last_index) return
-        nav_act.className += " active"
-        nav[last_index].className = "nav"
-        last_index = i
-        for (let j = 0; j < pages.length; j++) {
-            const page = pages[j]
-            page.style = `top : -${i * 100 + 5}%`
-        
-        }
+        changePage(i)
     }
 }
 
+const changePage = (index) => {
+    let nav_act = nav[index];
+    nav_act.className += " active"
+    nav[last_index].className = "nav"
+    last_index = index
+    for (let j = 0; j < pages.length; j++) {
+        const page = pages[j]
+        page.style = `top : -${index * 100 + 5}%`
+    
+    }
+}
+
+window.onwheel = (e) => {
+    if(e.wheelDeltaY > 0) {
+        if(last_index > 0) {
+            changePage(last_index - 1)
+        }
+    }else if(e.wheelDeltaY < 0){
+        if(last_index < pages.length - 1) {
+            changePage(last_index + 1)
+        }
+    }
+}
